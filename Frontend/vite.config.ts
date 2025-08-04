019891f950cf55e13ed9d6b33c8b6c09b3d229c5
vishnu <vishnuparihar239925@gmail.com>
2025-08-04 16:02:14 +0530

    personal finance tracker

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,11 +1,39 @@
- import path from "path"
+import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa"
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt"],
+      manifest: {
+        name: "Finance Tracker",
+        short_name: "FTB",
+        description: "Track your expenses and income easily!",
+        theme_color: "#6A1B9A",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
