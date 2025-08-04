c063ff151ec79a343a3bfe289046e654423e90a3
vishnu <vishnuparihar239925@gmail.com>
2025-08-04 15:44:07 +0530

    personal finance tracker

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -4,5 +4,5 @@ from models.transaction import Transaction
 from beanie import init_beanie
 
 async def init_db():
-    client = AsyncIOMotorClient("mongodb://localhost:27017")
+    client = AsyncIOMotorClient("mongodb+srv://vishnuparihar239925:1234@cluster0.oloj4sd.mongodb.net/")
     await init_beanie(database=client.finance_tracker, document_models=[SignupUser, Transaction])
